047621ebbbe3c6655597a099039817a95f684d29
Kim-jeongmin <55871387+Kim-jeongmin@users.noreply.github.com>
2020-07-11 21:32:59 +0900

    BOJ 5086 배수와 약수

---
+++ 구현/5086.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	while (true) {
+		int a, b; cin >> a >> b;
+		if (a == 0 && b == 0) break;
+		if (a > b && a%b == 0) cout << "multiple" << '\n';
+		else if (a < b && b%a == 0) cout << "factor" << '\n';
+		else cout << "neither\n";
+	}
+}
