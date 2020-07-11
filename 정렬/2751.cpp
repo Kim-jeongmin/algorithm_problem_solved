6a13d70a2851df46694ed42c582f21ebbbb7d873
Kim-jeongmin <55871387+Kim-jeongmin@users.noreply.github.com>
2020-07-11 18:19:27 +0900

    BOJ 2751 수 정렬하기 2

---
+++ 정렬/2751.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int k;
+	cin >> k;
+	vector<int> v(k);
+	for (int i = 0; i < k; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < k; i++) {
+		cout << v[i] << '\n';
+	}
+	return 0;
+}
