e0026d166bb6a4f5508b5f2f3d036dd8aa1c5fc4
Kim-jeongmin <55871387+Kim-jeongmin@users.noreply.github.com>
2020-07-12 15:54:59 +0900

    BOJ 11365 !밀비 급일

---
+++ 구현/11365.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	while (true) {
+		string str;  getline(cin, str);
+		if (str == "END") break;
+		reverse(str.begin(), str.end());
+		cout << str << '\n';
+	}
+}
