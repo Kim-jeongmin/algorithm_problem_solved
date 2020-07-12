d17bae35e862926ffad5f083146daf13af993bbe
Kim-jeongmin <55871387+Kim-jeongmin@users.noreply.github.com>
2020-07-12 15:41:08 +0900

    BOJ 10973 이전 순열

---
+++ 수학/10973.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int a; cin >> a;
+	int v[100001];
+	for (int i = 0; i < a; i++) cin >> v[i];
+	for (int i = a - 2; i >= 0; i--) {
+		if (v[i] > v[i + 1]) {
+			int num = 0, pos;
+			for (int j = i; j < a; j++) {
+				if (v[i] > v[j] && v[j] > num) {
+					num = v[j];
+					pos = j;
+				}
+			}
+			swap(v[i], v[pos]);
+			reverse(v + i + 1, v + a);
+			for (int i = 0; i < a; i++) {
+				cout << v[i] << ' ';
+			}
+			return 0;
+		}
+	}
+	cout << -1;
+}
