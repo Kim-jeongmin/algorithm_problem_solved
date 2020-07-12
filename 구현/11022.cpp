b4ff706ecdc8c221ed22a71dd46534e4515bd44c
Kim-jeongmin <55871387+Kim-jeongmin@users.noreply.github.com>
2020-07-12 15:47:18 +0900

    BOJ 11022 A+B - 8

---
+++ 구현/11022.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a; cin >> a;
+	for (int i = 0; i < a; i++) {
+		int p, q; cin >> p >> q;
+		cout << "Case #" << i + 1 << ": " << p << " + " << q << " = " << p + q << '\n';
+	}
+}
