51523186eda2d7f634602a7ad5cc6f88c9132d2a
김 정민 <chaeaok12@gmail.com>
2020-11-28 07:56:48 +0000

    BOJ 1010 다리 놓기

---
+++ 다이나믹 프로그래밍/1010.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  long long dp[31][31];
+  for(int i=1;i<31;i++){
+    for(int j=1;j<=i;j++){
+      if(i == j) {
+        dp[i][j] = 1;
+        continue;
+      }
+      if(i == 1) dp[i][j] = j;
+      else if(j == 1) dp[i][j] = i;
+      else {
+        dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+      }
+    }
+  }
+  int cnt; cin >> cnt;
+  while(cnt--){
+    int x,y; cin >> x >> y;
+    if(x > y) cout << dp[x][y] << '\n';
+    else cout << dp[y][x] << '\n';
+  }
+}
