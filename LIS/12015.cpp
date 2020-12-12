f0a2383316572124ee8a02d0ed72f77997e1dbb4
김 정민 <chaeaok12@gmail.com>
2020-12-12 05:04:24 +0000

    BOJ 12015 가장 긴 증가하는 부분수열 2

---
+++ LIS/12015.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int num; cin >> num;
+  int cnt = 0;
+  vector<int> v(num);
+  vector<int> vt(num);
+
+  for(int i=0;i<num;i++){
+    cin >> v[i];
+  }
+
+  vt.push_back(-1);
+
+  for(int i=0;i<num;i++){
+    if(vt.back() < v[i]){
+      vt.push_back(v[i]);
+      cnt++;
+    }
+    else {
+      auto it = lower_bound(vt.begin(), vt.end(), v[i]);
+      *it = v[i];
+    }
+  }
+
+  cout << cnt;
+}
