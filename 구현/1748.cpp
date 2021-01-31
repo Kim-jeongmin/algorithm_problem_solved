ec41b1e9be98ca34b00f07a4083099fa0e57721d
김 정민 <chaeaok12@gmail.com>
2021-01-31 09:37:54 +0000

    BOJ 1748 수 이어 쓰기 1

---
+++ 구현/1748.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N; cin >> N;
+  int tmp = N;
+  long long cnt = 0, result = 0;
+
+  while(N /= 10) cnt++;
+
+  for(int i=1;i<=cnt;i++) result += 9 * i * pow(10,i-1);
+
+  tmp -= pow(10,cnt) - 1;
+
+  result += tmp * (cnt+1);
+  
+
+  
+  cout << result;
+
+}
