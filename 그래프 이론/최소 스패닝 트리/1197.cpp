d13b16ab5301253651619dde981c20700430deef
김 정민 <chaeaok12@gmail.com>
2021-09-10 01:53:30 +0000

    BOJ 1197 최소 스패닝 트리

--- 그래프 이론/최소 스패닝 트리/1197.cpp
+++ 그래프 이론/최소 스패닝 트리/1197.cpp
@@ -1,4 +1,68 @@
+//크루스칼 알고리즘
 #include <bits/stdc++.h>
+using namespace std;
+
+int V, E;
+int parent[10001];
+vector<tuple<int,int,int>> graph;
+bool flag;
+long long result = 0;
+
+int find_parent(int x){
+  if(parent[x] == x) return x;
+  else return find_parent(parent[x]);
+}
+
+void unionVertex(int u, int v){
+  flag = false;
+
+  u = find_parent(u);
+  v = find_parent(v);
+
+  if(u == v) return;
+  else {
+    parent[u] = v;
+    flag = true;
+  }
+}
+
+int main(){
+
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  cin >> V >> E; 
+
+  for(int i=1;i<=V;i++) parent[i] = i;
+  
+
+  for(int i=0;i<E;i++){
+    int A, B, C; cin >>  A >> B >> C;
+    graph.push_back({C,A,B});
+  }
+
+  sort(graph.begin(), graph.end());
+
+  for(int i=0;i<E;i++){
+
+    unionVertex(get<1>(graph[i]), get<2>(graph[i]));
+
+    if(flag) result += get<0>(graph[i]);
+  }
+
+  cout << result;
+
+}
+
+
+
+
+
+
+
+
+//프림 알고리즘
+#include <bits/stdc++.h> 
 using namespace std;
 
 int V, E, A, B, C;
