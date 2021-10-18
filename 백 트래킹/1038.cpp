00f0541da7298326c3b6a73afc07e66c4bd4b24f
김 정민 <chaeaok12@gmail.com>
2021-10-18 22:35:31 +0000

    BOJ 1038 감소하는 수

---
+++ 백 트래킹/1038.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+vector<long long> v;
+
+void dfs(string str){
+    
+    if(str != "") {
+        long long n = stoll(str);
+        v.push_back(n);
+    }
+    
+    
+    for(int i=0;i<10;i++){
+        string num = to_string(i);
+    
+        if(i > str[0]-'0') dfs(num+str);
+        
+    }
+    
+}
+
+
+int main(){
+    cin >> N;
+    
+    dfs("");
+    
+    sort(v.begin(),v.end());
+    
+    if(N >= v.size()) cout << -1;
+    else cout << v[N];
+}
