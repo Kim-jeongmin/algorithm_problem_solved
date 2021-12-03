d4d90fd43e2ccf7ce54dcc1189fb25827375dd36
김 정민 <chaeaok12@gmail.com>
2021-12-03 13:50:29 +0000

    BOJ 1432 행복한 문자열

---
+++ 백 트래킹/1432.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[26];
+int result = 0;
+int N;
+
+bool isTrue(string str){
+	
+	bool flag = true;
+	
+	for(int i=0;i<str.size()-1;i++){
+		if(str[i] == str[i+1]) flag =false;
+	}
+	
+	return flag;
+}
+
+void dfs(int pos, string str){
+	
+	if(pos == N){
+		if(isTrue(str)){
+			result ++;
+		}
+		
+		return;
+	}
+	
+	for(int i=0;i<26;i++){
+		if(arr[i]) {
+			str += char(i+'a');
+		
+			arr[i] --;
+		
+			dfs(pos+1, str);
+		
+			str = str.substr(0,str.length()-1);
+			arr[i]++;
+		}
+	}
+}
+
+int main(){
+	string str; cin >> str;
+	N = str.size();
+	
+	for(int i=0;i<str.size();i++){
+		arr[str[i] - 'a']++; 
+	}
+	
+	dfs(0, "");
+	
+	cout << result;
+}
