8f5e806a70b3daada0302fb338472e72f3df4ca6
Kim-jeongmin <jmkimmessi@gmail.com>
2022-07-17 14:46:48 +0900

    BOJ 1256 사전

--- 수학/1256.cpp
+++ 수학/1256.cpp
@@ -2,21 +2,19 @@
 #define ll long long
 using namespace std;
 
+const ll MAX = 1000000000;
 ll dp[101][101];
 
 int main(){
     ll N, M, K; cin >> N >> M >> K;
-    vector<char> v;
 
-    for(int i=0;i<N;i++) v.push_back('a');
-    for(int i=0;i<M;i++) v.push_back('z');
-
-    for(int i=1;i<=M;i++) dp[1][i] = i+1;
-    for(int i=1;i<=N;i++) dp[i][1] = i+1;
+    for(int i=0;i<=100;i++) dp[0][i] = dp[i][0] = 1;
+    for(int i=1;i<=100;i++) dp[1][i] = dp[i][1] = i+1;
 
     for(int i=2;i<=N;i++){
         for(int j=2;j<=M;j++){
-            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            if(MAX < dp[i-1][j] + dp[i][j-1]) dp[i][j] = MAX;
+            else dp[i][j] = dp[i-1][j] + dp[i][j-1];
         }
     }
 
@@ -24,25 +22,25 @@ int main(){
         cout << -1;
     }
     else{
-        int k = 1;
-        while(k != K){
-            next_permutation(v.begin(), v.end());
-            k++;
-        }
-
         
-        for(int i=0;i<v.size();i++){
-            cout << v[i];
+        while(N!=0 && M!=0){
+            if(dp[N-1][M] < K){
+                cout << 'z';
+                K -= dp[N-1][M--];
+            }
+            else{
+                cout << 'a';
+                N--;
+            }
         }
         
+        if(N != 0){
+            for(int i=0;i<N;i++) cout << 'a';
+        }
+        else if(M != 0){
+            for(int i=0;i<M;i++) cout << 'z';
+        }
+    
     }
 
-    /*
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=M;j++){
-            cout << dp[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-    */
 }
