58d75f3eb1f3530dbe746ef4cb1e45bede73e1a2
Kim-jeongmin <jmkimmessi@gmail.com>
2022-08-05 16:10:57 +0900

    BOJ 12931 두 배 더하기

--- 그리디 알고리즘/12931.cpp
+++ 그리디 알고리즘/12931.cpp
@@ -1,15 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int N; cin >> N;
+int N, result = 0;
+int arr[51];
 
-    vector<int> v_1(N);
-    vector<int> v_2(N);
+bool is_zero(){
+
+    bool flag = true;
 
     for(int i=0;i<N;i++){
-        cin >> v_1[i];
+        if(arr[i]) {
+            flag = false;
+            break;
+        }
     }
 
+    return flag;
+}
+
+int main(){
+    cin >> N;
+    
+    for(int i=0;i<N;i++) cin >> arr[i];
+    
+    while(1){
+
+        if(is_zero()) {
+            cout << result;
+            break;
+        }
+
+        bool div_exist = true;
+        for(int i=0;i<N;i++) {
+            if(arr[i]%2){
+                div_exist = false;
+                break;
+            }
+        }
+
+        if(div_exist){
+            for(int i=0;i<N;i++){
+                arr[i] /= 2;
+            }
+            result++;
+        }
+        else{
+            for(int i=0;i<N;i++){
+                if(arr[i]%2){
+                    result++;
+                    arr[i]--;
+                }
+            }
+        }
+
+
+    }
     
 }
